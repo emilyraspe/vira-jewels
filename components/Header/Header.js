5583f985d8128ad62faf08f9f2fa4b9d1cf30ce8
Emily Raspe <emilyraspe@icloud.com>
2024-05-21 12:17:41 +0200

    header styling

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,8 +4,10 @@ export default function Header() {
   return (
     <div className="header">
       <Nav />
-      <h1>Vira Jewels</h1>
-      <p>By Khushi Kotahwala</p>
+      <div className="header--items">
+        <h1>Vira Jewels</h1>
+        <p className="by">By Khushi Kotahwala</p>
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import "@/components/Nav/Nav.css";
+import "@/components/Header/Header.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
