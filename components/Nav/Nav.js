c2440d3336eeba2ce3c3fb131e5c42dc91bb4355
Emily Raspe <emilyraspe@icloud.com>
2024-05-21 12:22:23 +0200

    add links

--- components/Nav/Nav.js
+++ components/Nav/Nav.js
@@ -1,12 +1,23 @@
 export default function Nav() {
   return (
     <nav>
-      <img src="/Icon-weiß.png" alt="Icon Weiß" height={50} />
+      <a href="#">
+        {" "}
+        <img src="/Icon-weiß.png" alt="Icon Weiß" height={50} />
+      </a>
       <ul className="nav--list">
-        <li className="nav--item">Über uns</li>
-        <li className="nav--item">Geschichte</li>
-        <li className="nav--item">Werte</li>
-        <li className="nav--item">Shops</li>
+        <li className="nav--item">
+          <a href="#ueber-uns">Über uns</a>
+        </li>
+        <li className="nav--item">
+          <a href="#geschichte">Geschichte</a>
+        </li>
+        <li className="nav--item">
+          <a href="#werte">Werte</a>
+        </li>
+        <li className="nav--item">
+          <a href="#shops">Shops</a>
+        </li>
       </ul>
     </nav>
   );
