4dc53c3515c7f8d48542780b38a86ef20886df0d
Emily Raspe <emilyraspe@icloud.com>
2024-05-21 14:51:54 +0200

    horizonal scroll

--- components/Left/Left.js
+++ components/Left/Left.js
@@ -1,8 +1,43 @@
+import Products from "../Products/Products";
+
 export default function Left() {
+  const products = [
+    {
+      name: "Blue Topaz Bracelet",
+      imgSrc: "/blue-topaz-bracelet.png",
+    },
+    {
+      name: "Amethyst Ring",
+      imgSrc: "/amethyst-ring.png",
+    },
+    {
+      name: "Peridot Set",
+      imgSrc: "/peridot-set.png",
+    },
+    {
+      name: "Smoky Quartz Earrings",
+      imgSrc: "/smoky-quartz-earrings.png",
+    },
+    {
+      name: "Tanzanite Earrings",
+      imgSrc: "/tanzanite-earrings.png",
+    },
+  ];
+
   return (
     <div className="left">
       <h3>Products</h3>
-      <p></p>
+      <div className="left--scroll">
+        {products.map((product, index) => {
+          return (
+            <Products
+              key={index}
+              productName={product.name}
+              imgSrc={product.imgSrc}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import "@/components/Nav/Nav.css";
 import "@/components/Header/Header.css";
+import "@/components/Products/Products.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
