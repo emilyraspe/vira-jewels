c6f6d69fe94b197b10675ba5f9a09e90f8b16736
Emily Raspe <emilyraspe@icloud.com>
2024-05-21 15:47:20 +0200

    edit right side

---
+++ components/About/About.js
@@ -0,0 +1,51 @@
+export default function About() {
+  return (
+    <>
+      <p id="ueber-uns" className="tagline">
+        Über uns
+      </p>
+      <h2>Tauchen Sie ein in die faszinierende Welt von Vira Jewels</h2>
+      <p>
+        Tauchen Sie ein in die faszinierende Welt von Vira Jewels, wo
+        authentische Schönheit auf zeitlose Eleganz trifft. Wir sind stolz
+        darauf, Ihnen hochwertigen Edelsteinschmuck anzubieten, der mit
+        unübertroffener Handwerkskunst gefertigt wird. Unsere visionäre
+        Inhaberin, Khushi Kotahwala, verkörpert die Essenz von Vira Jewels -
+        eine kreative Visionärin mit tiefen Wurzeln in der Welt der Edelsteine.
+        Ursprünglich stammt ihre Familie aus der prachtvollen Stadt Jaipur in
+        Indien, fließt in Khushis Adern die Leidenschaft für Schmuckdesign seit
+        Generationen. In Bremen geboren und aufgewachsen und mit einem
+        Bachelor-Abschluss in Wirtschaft und Psychologie aus London verbindet
+        Khushi geschickt die künstlerische Seite des Schmuckhandwerks mit einem
+        tiefen Verständnis für die Bedeutung hinter jedem wertvollen Stück.
+        <br />
+        Vira Jewels ist das Ergebnis einer langen Familientradition, die bis ins
+        Jahr 1727 zurückreicht. Es ist die Verkörperung von Khushis moderner
+        Vision, Echtschmuck ohne Kompromisse anzubieten. Unsere Mission ist es,
+        Ihnen Schmuckstücke zu präsentieren, die ausschließlich aus echten
+        Edelsteinen, Edelmetallen und mit echter Handarbeit gefertigt werden.
+        "Alles Echt - ohne Ausnahme." Dies ist unser Leitspruch und das
+        Versprechen, das wir Ihnen geben. Bei Vira Jewels können Sie sicher
+        sein, dass Sie in jedem Stück echte Qualität und echte Schönheit
+        erhalten. Wir setzen auf Ehrlichkeit und Transparenz, denn Ihre
+        Zufriedenheit ist unser höchstes Anliegen.
+        <br />
+        Jedes Schmuckstück in unserer Kollektion wurde von Khushis talentierten
+        Designern entworfen. Ihre Leidenschaft und Hingabe spiegeln sich in
+        jedem Detail wider. Zeitlosigkeit ist unser Anspruch - unsere Stücke
+        sind dafür gemacht, Generationen zu überdauern. Sie werden nicht nur
+        eine ästhetische Bereicherung für Ihre Schmuckschatulle sein, sondern
+        auch Erinnerungen und Emotionen für die kommenden Jahre bewahren. Wir
+        laden Sie herzlich ein, die fesselnde Welt von Vira Jewels zu entdecken.
+        Lassen Sie sich von der Schönheit der Edelsteine verzaubern, von der
+        meisterhaften Handwerkskunst begeistern und von der Geschichte jedes
+        Schmuckstücks berühren. Wir sind stolz darauf, ein Teil Ihrer besonderen
+        Momente zu sein und Ihnen einzigartige Schmuckstücke zu bieten, die Sie
+        für immer schätzen werden. Willkommen bei Vira Jewels - wo Eleganz auf
+        Echtheit trifft und die Schönheit der Natur in jedem Stück zum Leben
+        erweckt wird. Treten Sie ein in unsere Welt des Echtschmucks und lassen
+        Sie sich von der Magie verführen.
+      </p>
+    </>
+  );
+}
---
+++ components/History/History.js
@@ -0,0 +1,41 @@
+export default function History() {
+  return (
+    <>
+      <p id="geschichte" className="tagline">
+        Unsere Geschichte
+      </p>
+      <h2>Die Geschichte von Vira Jewels</h2>
+      <p>
+        Die Familie Kotahwala ist untrennbar mit der Geschichte von Jaipur und
+        der Schmuckherstellung seit der Gründung im Jahre 1727 verbunden.
+        Ursprünglich aus Kota in Indien stammend, wurde die Familie von Maharaja
+        Sawai Jai Singh II nach Jaipur eingeladen, um dort ihre herausragende
+        Handwerkskunst auszuüben. Von diesem Moment an wurde der Name der
+        Familie zu "Kotahwala" - was so viel bedeutet wie "aus Kota stammend".
+        Seit 1727 verarbeitet die Familie Kotahwala Schmuck in Jaipur, das für
+        seine reichen Edelsteinvorkommen bekannt ist. Im Jahr 1997 gründete Uday
+        Kotahwala in Bremen, Deutschland, eine Firma, um die feinen Edelsteine
+        aus Jaipur in Deutschland und Europa zu vertreiben. Im Laufe der Zeit
+        erweiterte die Familie ihr Angebot um Echtschmuck, besetzt mit den
+        hochwertigen Edelsteinen aus ihrer Heimat. Als Edelmetall verwendeten
+        sie 925 Silber oder Gold in den Legierungen 375, 585 oder 750.
+        <br />
+        Im Jahr 2022 entschied sich die Tochter Khushi Kotahwala, eine eigene
+        Firma zu gründen, um ihre Vision von modernem Edelsteinschmuck
+        umzusetzen. In Bremen geboren und aufgewachsen, absolvierte sie ein
+        Studium der Wirtschaft und Psychologie in London mit einem
+        Bachelor-Abschluss. Khushi Kotahwala vereint ihre indischen Wurzeln mit
+        der langen Familientradition in der Schmuckherstellung und schafft somit
+        das Fundament für ihre eigene Marke Vira Jewels. Ihre Schmuckstücke
+        werden unter dieser europaweit registrierten Marke angeboten und führen
+        somit eine unfassbar lange Tradition erfolgreich weiter. Die Familie
+        Kotahwala ist stolz darauf, seit der Gründung von Jaipur im Jahr 1727
+        Teil dieser faszinierenden Stadt und ihrer blühenden Schmuckindustrie zu
+        sein. Mit ihrem umfangreichen Know-how in der Verarbeitung feinster
+        Edelsteine und der Liebe zum Detail haben sie sich zu einer der
+        bedeutendsten Schmuckdynastien entwickelt, deren Erbe und Handwerkskunst
+        nun in den zeitlosen Kreationen von Vira Jewels lebendig werden.
+      </p>
+    </>
+  );
+}
