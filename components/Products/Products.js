d877976d812a5479713d694ed6e5a78198b845de
Emily Raspe <emilyraspe@icloud.com>
2024-05-22 15:58:06 +0200

    small adjustments

--- components/Products/Products.js
+++ components/Products/Products.js
@@ -3,7 +3,7 @@ export default function Products({ productName, imgSrc }) {
     <div className="product--container">
       {" "}
       <img src={imgSrc} alt={productName} className="product--img" />
-      <p className="product--name">{productName}</p>
+      <p className="product--name">– {productName} – </p>
     </div>
   );
 }
