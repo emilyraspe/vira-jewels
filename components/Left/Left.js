0b36918978b22a5e794227101ec356caecfd02ff
Emily Raspe <emilyraspe@icloud.com>
2024-05-23 17:33:01 +0200

    button functionality

--- components/Left/Left.js
+++ components/Left/Left.js
@@ -1,5 +1,6 @@
 import Products from "../Products/Products";
 import Header from "../Header/Header";
+import { useRef, useState, useEffect } from "react";
 
 export default function Left() {
   const products = [
@@ -25,24 +26,77 @@ export default function Left() {
     },
   ];
 
+  const elementRef = useRef(null);
+  const productRef = useRef(null);
+
+  const [arrowDisable, setArrowDisable] = useState(true);
+  const [productWidth, setProductWidth] = useState(0);
+
+  useEffect(() => {
+    if (productRef.current) {
+      setProductWidth(productRef.current.offsetWidth);
+    }
+  }, []);
+
+  const handleHorizantalScroll = (element, speed, distance, step) => {
+    let scrollAmount = 0;
+    const slideTimer = setInterval(() => {
+      element.scrollLeft += step;
+      scrollAmount += Math.abs(step);
+      if (scrollAmount >= distance) {
+        clearInterval(slideTimer);
+      }
+      if (element.scrollLeft === 0) {
+        setArrowDisable(true);
+      } else {
+        setArrowDisable(false);
+      }
+    }, speed);
+  };
+
   return (
     <div className="left">
       <Header />
       <div className="product">
         <h3>Products</h3>
-        <div className="left--scroll">
+        <div className="left--scroll" ref={elementRef}>
           {products.map((product, index) => {
             return (
               <Products
-                key={index}
                 productName={product.name}
                 imgSrc={product.imgSrc}
+                productRef={productRef}
               />
             );
           })}
         </div>
-        <button className="products--button">←</button>
-        <button className="products--button">→</button>
+        <button
+          className="products--button"
+          onClick={() => {
+            handleHorizantalScroll(
+              elementRef.current,
+              25,
+              productWidth,
+              -productWidth
+            );
+          }}
+          disabled={arrowDisable}
+        >
+          ←
+        </button>
+        <button
+          className="products--button"
+          onClick={() => {
+            handleHorizantalScroll(
+              elementRef.current,
+              25,
+              productWidth,
+              productWidth
+            );
+          }}
+        >
+          →
+        </button>
       </div>
     </div>
   );
--- components/Products/Products.js
+++ components/Products/Products.js
@@ -1,6 +1,6 @@
-export default function Products({ productName, imgSrc }) {
+export default function Products({ productName, imgSrc, productRef }) {
   return (
-    <div className="product--container">
+    <div className="product--container" ref={productRef}>
       {" "}
       <img src={imgSrc} alt={productName} className="product--img" />
       <p className="product--name">– {productName} – </p>
