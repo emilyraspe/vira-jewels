774394029ddde26b5fe80427a00bb5c4696c6e68
Emily Raspe <emilyraspe@icloud.com>
2024-05-27 18:17:12 +0200

    fix typo

--- components/Values/Values.js
+++ components/Values/Values.js
@@ -20,39 +20,40 @@ export default function Values({ valuesRef }) {
         Geschichten erzählt. Ihre Geschichte.
       </p>
       <h3>Unsere Top 3 Werte</h3>
+
       <div className="werte--container">
-        <h4>Unvergleichliche Qualität</h4>
         <p>
-          Unsere Preziosen werden mit viel Liebe und höchster Handwerkskunst aus
-          925er Silber in verschiedenen Veredelungen handgefertigt. Als Besatz
-          verwenden wir ausschließlich nur echte Edelsteine und/oder Diamanten.
-          Die verwendeten Edelsteine überzeugen durch ihr Feuer, ihre
-          Transparenz und dem hochfeinen Schliff. Unser Ziel ist es, mit Hilfe
-          unseres Expertenteams aus erfahrenen Edelsteinschleifern,
-          Schmuck-designern und Juwelieren, unseren Kunden nur Schmuck von
-          höchster Qualität zu präsentieren.
+          <strong>Unvergleichliche Qualität:</strong> Unsere Preziosen werden
+          mit viel Liebe und höchster Handwerkskunst aus 925er Silber in
+          verschiedenen Veredelungen handgefertigt. Als Besatz verwenden wir
+          ausschließlich nur echte Edelsteine und/oder Diamanten. Die
+          verwendeten Edelsteine überzeugen durch ihr Feuer, ihre Transparenz
+          und dem hochfeinen Schliff. Unser Ziel ist es, mit Hilfe unseres
+          Expertenteams aus erfahrenen Edelsteinschleifern, Schmuck-designern
+          und Juwelieren, unseren Kunden nur Schmuck von höchster Qualität zu
+          präsentieren.
         </p>
       </div>
 
       <div className="werte--container">
-        <h4>Äußerst zuverlässig</h4>
         <p>
-          Die Leidenschaft und Begeisterung für das Edelsteinschleifen, den
-          Edelsteinhandel und die Schmuckherstellung wurde über viele
-          Generationen weitergegeben und daher können Sie sich bei jedem
-          Schmuckstück auf Exzellenz verlassen. Wir kümmern uns um die Details,
-          damit Sie Ihren Schmuck mit Stolz tragen können.
+          <strong>Äußerst zuverlässig:</strong> Die Leidenschaft und
+          Begeisterung für das Edelsteinschleifen, den Edelsteinhandel und die
+          Schmuckherstellung wurde über viele Generationen weitergegeben und
+          daher können Sie sich bei jedem Schmuckstück auf Exzellenz verlassen.
+          Wir kümmern uns um die Details, damit Sie Ihren Schmuck mit Stolz
+          tragen können.
         </p>
       </div>
 
       <div className="werte--container">
-        <h4>Für jeden erschwinglich</h4>
         <p>
-          Echter Schmuck sollte für jeden zugänglich sein. Mit faszinierenden
-          Edelsteinen in verschiedenen Farben, Formen und Größen können wir
-          Farbe und Freude in den Alltag bringen. Echtschmuck muss nicht nur zu
-          besonderen Anlässen getragen werden. Vira Jewels macht das möglich und
-          bietet ein unschlagbares Preisleistungsverhältnis.
+          <strong>Für jeden erschwinglich:</strong> Echter Schmuck sollte für
+          jeden zugänglich sein. Mit faszinierenden Edelsteinen in verschiedenen
+          Farben, Formen und Größen können wir Farbe und Freude in den Alltag
+          bringen. Echtschmuck muss nicht nur zu besonderen Anlässen getragen
+          werden. Vira Jewels macht das möglich und bietet ein unschlagbares
+          Preisleistungsverhältnis.
         </p>
       </div>
     </div>
