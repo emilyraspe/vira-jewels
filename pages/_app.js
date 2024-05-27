5fe981e8f1d7bc875da0cd7064bd2bccc5086ae3
Emily Raspe <emilyraspe@icloud.com>
2024-05-27 18:36:58 +0200

    styling for shops list

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "@/components/Nav/Nav.css";
 import "@/components/Header/Header.css";
 import "@/components/Products/Products.css";
 import "@/components/Values/Values.css";
+import "@/components/Shops/Shops.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
